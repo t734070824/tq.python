b88e90d3628244a0c80c1f866009658fa214177d
tangqing <734070824@qq.com>
2019-02-21 14:07:59 +0800

    变量

---
+++ tq.python/study/book/_1_variable_and_data_type/VariableAndDataType.py
@@ -0,0 +1,37 @@
+message = "hello world!"
+print(message)
+
+# 字符串
+# 修改大小写
+name='ada lioe'
+print(name.title())
+name='aDa lIe'
+print(name.title())
+
+print(name.upper())
+print(name.lower())
+
+'''
+删除空白 末尾
+'''
+name = "  aaa  asda***"
+print(name.rstrip("*"))
+print(name)
+
+'''
+开头
+'''
+name = 'sssaaasddss'
+print(name.lstrip('s'))
+
+'''
+两边
+'''
+name = 'aa ssaaa'
+print(name.strip('a'))
+
+# 数字
+
+'''
+
+'''
